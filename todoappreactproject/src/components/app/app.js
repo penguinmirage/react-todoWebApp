425b0ae55240a917ab5cb82878dc7b0280b8af62
Petr Chukhnov <peka4u@vk.com>
2024-10-29 20:59:05 +0300

    Created timer functionality with localstorage

--- todoappreactproject/src/components/app/app.js
+++ todoappreactproject/src/components/app/app.js
@@ -1,55 +1,88 @@
-import React, { Component } from "react";
-import AppHeader from "../app-header/";
-import TaskList from "../task-list";
-import TasksFilter from "../tasks-filter";
-import NewTaskForm from "../new-task-form";
-import Footer from "../app-footer";
-import "./app.css";
-import PropTypes from "prop-types";
+import React, { Component } from 'react';
+import AppHeader from '../app-header/';
+import TaskList from '../task-list';
+import TasksFilter from '../tasks-filter';
+import NewTaskForm from '../new-task-form';
+import Footer from '../app-footer';
+import './app.css';
+import PropTypes from 'prop-types';
+import { v4 as uuidv4 } from 'uuid';
 
 export default class App extends Component {
   maxId = 100;
 
   state = {
     todoData: [],
-    term: "",
-    filter: "",
+    term: '',
+    filter: '',
   };
 
+  //updated 29-10-2024
   createTodoItem(label) {
     return {
       label,
       important: false,
       done: false,
-      id: this.maxId++,
+      id: uuidv4(),
     };
   }
 
   deleteItem = (id) => {
-    console.log(id);
     this.setState(({ todoData }) => {
-      const idx = todoData.findIndex((el) => el.id === id);
-      todoData.splice(idx, 1);
-
-      const before = todoData.slice(0, idx);
-      const after = todoData.slice(idx + 1);
-      const newArray = [...before, ...after];
-
-      return {
-        todoData: newArray,
-      };
+      const newArray = todoData.filter((item) => item.id !== id);
+      return { todoData: newArray };
     });
+    localStorage.removeItem(`timer-${id}`); // Optional: Clear timer data on deletion
+    localStorage.removeItem(`timerActive-${id}`);
   };
 
   addItem = (text) => {
-    const newItem = this.createTodoItem(text);
-    this.setState(({ todoData }) => {
-      const newArr = [...todoData, newItem];
-      return {
-        todoData: newArr,
-      };
-    });
+    const newItem = {
+      ...this.createTodoItem(text),
+      isNewTask: true,
+    };
+    this.setState(({ todoData }) => ({
+      todoData: [...todoData, newItem],
+    }));
   };
+
+  //   createTodoItem(label) {
+  //     return {
+  //       label,
+  //       important: false,
+  //       done: false,
+  //       id: this.maxId++,
+  //     };
+  //   }
+  //
+  //   deleteItem = (id) => {
+  //     console.log(id);
+  //     this.setState(({ todoData }) => {
+  //       const idx = todoData.findIndex((el) => el.id === id);
+  //       todoData.splice(idx, 1);
+  //
+  //       const before = todoData.slice(0, idx);
+  //       const after = todoData.slice(idx + 1);
+  //       const newArray = [...before, ...after];
+  //
+  //       return {
+  //         todoData: newArray,
+  //       };
+  //     });
+  //   };
+  //
+  //   addItem = (text) => {
+  //     const newItem = {
+  //       ...this.createTodoItem(text),
+  //       isNewTask: true,
+  //     };
+  //     this.setState(({ todoData }) => {
+  //       const newArr = [...todoData, newItem];
+  //       return {
+  //         todoData: newArr,
+  //       };
+  //     });
+  //   };
 
   editItem = (id, editedItem) => {
     this.setState(({ todoData }) => {
@@ -57,11 +90,7 @@ export default class App extends Component {
       todoData.splice(idx, 1);
       const oldDataFile = todoData[idx];
       const updatedDataFile = { ...oldDataFile, label: editedItem };
-      const updatedTodoDataArray = [
-        ...todoData.slice(0, idx),
-        updatedDataFile,
-        ...todoData.slice(idx + 1),
-      ];
+      const updatedTodoDataArray = [...todoData.slice(0, idx), updatedDataFile, ...todoData.slice(idx + 1)];
       return {
         todoData: updatedTodoDataArray,
       };
@@ -80,10 +109,10 @@ export default class App extends Component {
   onToggleDone = (id) => {
     this.setState(({ todoData }) => {
       return {
-        todoData: this.toggleProperty(todoData, id, "done"),
+        todoData: this.toggleProperty(todoData, id, 'done'),
       };
     });
-    console.log("Toggle done", id);
+    console.log('Toggle done', id);
   };
 
   search(items, term) {
@@ -102,11 +131,11 @@ export default class App extends Component {
 
   filter(items, filter) {
     switch (filter) {
-      case "all":
+      case 'all':
         return items;
-      case "active":
+      case 'active':
         return items.filter((item) => !item.done);
-      case "done":
+      case 'done':
         return items.filter((item) => item.done);
       default:
         return items;
@@ -140,16 +169,11 @@ export default class App extends Component {
           todos={visibleItems}
           onDeleted={this.deleteItem}
           onEdited={this.editItem}
-          onToggleImportant={this.onToggleImportant}
           onToggleDone={this.onToggleDone}
         />
         <div className="footer">
           <Footer count={todoCount} />
-          <TasksFilter
-            filter={filter}
-            onFilterChange={this.onFilterChange}
-            onCleared={this.onCleared}
-          />
+          <TasksFilter filter={filter} onFilterChange={this.onFilterChange} onCleared={this.onCleared} />
         </div>
       </div>
     );
@@ -158,7 +182,7 @@ export default class App extends Component {
 
 App.defaultProps = {
   todoData: [],
-  filter: "all",
+  filter: 'all',
 };
 
 App.propTypes = {
--- todoappreactproject/src/components/task/task.js
+++ todoappreactproject/src/components/task/task.js
@@ -4,19 +4,77 @@ import PropTypes from 'prop-types';
 import './task.css';
 
 export default class TodoListItem extends Component {
-  state = {
-    isEditing: false,
-    editedTask: this.props.label,
-    createdTime: new Date(),
-    editedTime: null,
+  constructor(props) {
+    super(props);
+
+    // Retrieve timer info from localStorage based on unique ID
+    const existingElapsedTime = parseInt(localStorage.getItem(`timer-${this.props.id}`)) || 0;
+    const existingTimerActive = JSON.parse(localStorage.getItem(`timerActive-${this.props.id}`)) || false;
+
+    this.state = {
+      isEditing: false,
+      editedTask: this.props.label,
+      createdTime: new Date(),
+      editedTime: null,
+      elapsedTime: this.props.isNewTask ? 0 : existingElapsedTime,
+      timerActive: this.props.isNewTask ? false : existingTimerActive,
+    };
+
+    this.intervalId = null;
+  }
+
+  componentDidMount() {
+    if (this.state.timerActive) {
+      this.startTimer();
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+    localStorage.setItem(`timer-${this.props.id}`, this.state.elapsedTime);
+    localStorage.setItem(`timerActive-${this.props.id}`, JSON.stringify(this.state.timerActive));
+  }
+
+  startTimer = () => {
+    if (!this.state.timerActive) {
+      this.intervalId = setInterval(() => {
+        this.setState((prevState) => {
+          const newElapsedTime = prevState.elapsedTime + 1;
+          localStorage.setItem(`timer-${this.props.id}`, newElapsedTime);
+          return { elapsedTime: newElapsedTime, timerActive: true };
+        });
+      }, 1000);
+    }
+  };
+
+  stopTimer = () => {
+    clearInterval(this.intervalId);
+    this.setState({ timerActive: false });
+    localStorage.setItem(`timerActive-${this.props.id}`, JSON.stringify(false));
+  };
+
+  resetTimer = () => {
+    clearInterval(this.intervalId);
+    this.setState({ elapsedTime: 0, timerActive: false });
+    localStorage.setItem(`timer-${this.props.id}`, 0);
+    localStorage.setItem(`timerActive-${this.props.id}`, JSON.stringify(false));
+  };
+
+  formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
+  };
+
+  handleToggleDone = () => {
+    this.props.onToggleDone(this.props.id);
+    if (!this.props.done) {
+      this.stopTimer();
+    }
   };
 
   beginEditing = () => {
-    this.setState({ isEditing: true }); // Enters the edit mode
-  };
-
-  whatIsEditing = (e) => {
-    this.setState({ editedTask: e.target.value }); // Works while we edit
+    this.setState({ isEditing: true });
   };
 
   saveChangesEditing = (e) => {
@@ -28,9 +86,8 @@ export default class TodoListItem extends Component {
       onEdited(editedTask);
       this.setState({ isEditing: false, editedTime: new Date() });
     }
-  }; // Exit edit mode
+  };
 
-  // Save changes if Enter key is pressed
   saveOnEnterKeyDown = (e) => {
     if (e.key === 'Enter') {
       this.saveChangesEditing(e);
@@ -39,81 +96,266 @@ export default class TodoListItem extends Component {
 
   createTimestamp = () => {
     const { createdTime, editedTime } = this.state;
-    if (editedTime) {
-      return `edited ${formatDistanceToNow(
-        new Date(editedTime),
-        { includeSeconds: true },
-        {
-          addSuffix: true,
-        }
-      )}`;
-    } else {
-      return `created ${formatDistanceToNow(
-        new Date(createdTime),
-        { includeSeconds: true },
-        {
-          addSuffix: true,
-        }
-      )}`;
-    }
+    return editedTime
+      ? `edited ${formatDistanceToNow(new Date(editedTime), { includeSeconds: true })}`
+      : `created ${formatDistanceToNow(new Date(createdTime), { includeSeconds: true })}`;
   };
 
   render() {
-    const { label, onDeleted, onToggleDone, done } = this.props;
-    const { isEditing, editedTask } = this.state;
+    const { label, onDeleted, done } = this.props;
+    const { isEditing, editedTask, elapsedTime, timerActive } = this.state;
 
     let classNames = 'todo-list-item description';
     if (done) {
       classNames += ' done completed';
     }
 
-    // для isEditing делаем тернарник: ?едитТаск :обычныйТаск
-
     return (
       <span className="todo-list">
-        <input className="toggle" type="checkbox" onChange={onToggleDone} checked={done} />
+        <input className="toggle" type="checkbox" onChange={this.handleToggleDone} checked={done} />
         {isEditing ? (
           <li className="editing">
             <input
               type="text"
               value={editedTask}
-              onChange={this.whatIsEditing}
-              onKeyDown={this.saveOnEnterKeyDown}
+              onChange={(e) => this.setState({ editedTask: e.target.value })}
+              onKeyDown={(e) => e.key === 'Enter' && this.saveChangesEditing(e)}
               className="edit"
-              onSubmit={this.saveChangesEditing}
               autoFocus
             />
           </li>
         ) : (
-          <label className="todo-list-item" onClick={onToggleDone}>
+          <label className="todo-list-item" onClick={this.handleToggleDone}>
             <span className={classNames}>{label}</span>
             <span className="created">{this.createTimestamp()}</span>
           </label>
         )}
 
-        <div type="button" className="btn" onClick={this.beginEditing}>
-          <i className="icon icon-edit" />
+        <div className="timer">
+          <span>{this.formatTime(elapsedTime)}</span>
+          <button onClick={timerActive ? this.stopTimer : this.startTimer} className="icon-timer">
+            {timerActive ? '⏸' : '▶'}
+          </button>
         </div>
 
-        <div type="button" className="btn" onClick={onDeleted}>
+        <li className="btn" onClick={() => this.setState({ isEditing: true })}>
+          <i className="icon icon-edit" />
+        </li>
+
+        <li className="btn" onClick={onDeleted}>
           <i className="icon icon-destroy" />
-        </div>
+        </li>
       </span>
     );
   }
 }
+
 TodoListItem.defaultProps = {
-  filter: 'all', // Default value for the filter prop
+  filter: 'all',
+  done: false,
+  isNewTask: false,
 };
 
 TodoListItem.propTypes = {
   filter: PropTypes.string,
-  label: PropTypes.string,
-  onEdited: PropTypes.object,
-  onDeleted: PropTypes.number,
-  onToggleDone: PropTypes.bool,
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onEdited: PropTypes.func.isRequired,
+  onDeleted: PropTypes.func.isRequired,
+  onToggleDone: PropTypes.func.isRequired,
   done: PropTypes.bool,
+  isNewTask: PropTypes.bool,
 };
+
+// import React, { Component } from 'react';
+// import { formatDistanceToNow } from 'date-fns';
+// import PropTypes from 'prop-types';
+// import './task.css';
+//
+// export default class TodoListItem extends Component {
+//   constructor(props) {
+//     super(props);
+//
+//     // Initialize state, check for existing timer values in local storage for each task
+//     const existingElapsedTime = parseInt(localStorage.getItem(`timer-${this.props.id}`)) || 0;
+//     const existingTimerActive = JSON.parse(localStorage.getItem(`timerActive-${this.props.id}`)) || false;
+//
+//     this.state = {
+//       isEditing: false,
+//       editedTask: this.props.label,
+//       createdTime: new Date(),
+//       editedTime: null,
+//       elapsedTime: this.props.isNewTask ? 0 : existingElapsedTime,
+//       timerActive: this.props.isNewTask ? false : existingTimerActive,
+//     };
+//
+//     this.intervalId = null;
+//   }
+//
+//   componentDidMount() {
+//     if (this.state.timerActive) {
+//       this.startTimer();
+//     }
+//   }
+//
+//   componentWillUnmount() {
+//     clearInterval(this.intervalId);
+//     // Save current elapsed time and timer state to local storage
+//     localStorage.setItem(`timer-${this.props.id}`, this.state.elapsedTime);
+//     localStorage.setItem(`timerActive-${this.props.id}`, JSON.stringify(this.state.timerActive));
+//   }
+//
+//   startTimer = () => {
+//     if (!this.state.timerActive) {
+//       this.intervalId = setInterval(() => {
+//         this.setState((prevState) => {
+//           const newElapsedTime = prevState.elapsedTime + 1;
+//           localStorage.setItem(`timer-${this.props.id}`, newElapsedTime);
+//           return { elapsedTime: newElapsedTime, timerActive: true };
+//         });
+//       }, 1000);
+//     }
+//   };
+//
+//   stopTimer = () => {
+//     clearInterval(this.intervalId);
+//     this.setState({ timerActive: false });
+//     localStorage.setItem(`timerActive-${this.props.id}`, JSON.stringify(false));
+//   };
+//
+//   resetTimer = () => {
+//     clearInterval(this.intervalId);
+//     this.setState({ elapsedTime: 0, timerActive: false });
+//     localStorage.setItem(`timer-${this.props.id}`, 0);
+//     localStorage.setItem(`timerActive-${this.props.id}`, JSON.stringify(false));
+//   };
+//
+//   formatTime = (seconds) => {
+//     const minutes = Math.floor(seconds / 60);
+//     const remainingSeconds = seconds % 60;
+//     return `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
+//   };
+//
+//   handleToggleDone = () => {
+//     this.props.onToggleDone(this.props.id);
+//     if (!this.props.done) {
+//       this.stopTimer();
+//     }
+//   };
+//
+//   // методы для редактироания тасков
+//
+//   beginEditing = () => {
+//     this.setState({ isEditing: true }); // Enters the edit mode
+//   };
+//
+//   whatIsEditing = (e) => {
+//     this.setState({ editedTask: e.target.value }); // Works while we edit
+//   };
+//
+//   saveChangesEditing = (e) => {
+//     e.preventDefault();
+//     const { editedTask } = this.state;
+//     const { onEdited } = this.props;
+//
+//     if (editedTask.trim()) {
+//       onEdited(editedTask);
+//       this.setState({ isEditing: false, editedTime: new Date() });
+//     }
+//   }; // Exit edit mode
+//
+//   // Save changes if Enter key is pressed
+//   saveOnEnterKeyDown = (e) => {
+//     if (e.key === 'Enter') {
+//       this.saveChangesEditing(e);
+//     }
+//   };
+//
+//   createTimestamp = () => {
+//     const { createdTime, editedTime } = this.state;
+//     if (editedTime) {
+//       return `edited ${formatDistanceToNow(
+//         new Date(editedTime),
+//         { includeSeconds: true },
+//         {
+//           addSuffix: true,
+//         }
+//       )}`;
+//     } else {
+//       return `created ${formatDistanceToNow(
+//         new Date(createdTime),
+//         { includeSeconds: true },
+//         {
+//           addSuffix: true,
+//         }
+//       )}`;
+//     }
+//   };
+//
+//   render() {
+//     const { label, onDeleted, done } = this.props;
+//     const { isEditing, editedTask, elapsedTime, timerActive } = this.state;
+//
+//     let classNames = 'todo-list-item description';
+//     if (done) {
+//       classNames += ' done completed';
+//     }
+//
+//     return (
+//       <span className="todo-list">
+//         <input className="toggle" type="checkbox" onChange={this.handleToggleDone} checked={done} />
+//         {isEditing ? (
+//           <li className="editing">
+//             <input
+//               type="text"
+//               value={editedTask}
+//               onChange={(e) => this.setState({ editedTask: e.target.value })}
+//               onKeyDown={(e) => e.key === 'Enter' && this.saveChangesEditing(e)}
+//               className="edit"
+//               autoFocus
+//             />
+//           </li>
+//         ) : (
+//           <label className="todo-list-item" onClick={this.handleToggleDone}>
+//             <span className={classNames}>{label}</span>
+//             <span className="created">{this.createTimestamp()}</span>
+//           </label>
+//         )}
+//
+//         <div className="timer">
+//           <span>{this.formatTime(elapsedTime)}</span>
+//           <button onClick={timerActive ? this.stopTimer : this.startTimer} className="icon-timer">
+//             {timerActive ? '⏸' : '▶'}
+//           </button>
+//         </div>
+//
+//         <li className="btn" onClick={() => this.setState({ isEditing: true })}>
+//           <i className="icon icon-edit" />
+//         </li>
+//
+//         <li className="btn" onClick={onDeleted}>
+//           <i className="icon icon-destroy" />
+//         </li>
+//       </span>
+//     );
+//   }
+// }
+// TodoListItem.defaultProps = {
+//   filter: 'all',
+//   done: false,
+//   isNewTask: false,
+// };
+//
+// TodoListItem.propTypes = {
+//   filter: PropTypes.string,
+//   id: PropTypes.string.isRequired,
+//   label: PropTypes.string.isRequired,
+//   onEdited: PropTypes.func.isRequired,
+//   onDeleted: PropTypes.func.isRequired,
+//   onToggleDone: PropTypes.func.isRequired,
+//   done: PropTypes.bool,
+//   isNewTask: PropTypes.bool,
+// };
 
 // export default class Task extends Component {
 //   state = {
